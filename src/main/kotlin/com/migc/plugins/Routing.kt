d15e182d99a804919091509b6e51cf4a2a75d66b
Miguel <migc.dev@gmail.com>
2022-08-24 17:59:07 -0700

    separated routing call in a new package

--- src/main/kotlin/com/migc/plugins/Routing.kt
+++ src/main/kotlin/com/migc/plugins/Routing.kt
@@ -1,15 +1,11 @@
 package com.migc.plugins
 
+import com.migc.routes.root
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        root()
     }
 }
